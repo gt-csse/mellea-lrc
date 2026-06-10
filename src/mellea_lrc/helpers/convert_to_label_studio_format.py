c36291746eed616a24317f913fc0faa793a907af
ADRian123L <adrian163@usf.edu>
2026-06-10 01:12:05 -0400

    feat(extraction): add function to assign IDs to citations

--- src/mellea_lrc/helpers/convert_to_label_studio_format.py
+++ src/mellea_lrc/helpers/convert_to_label_studio_format.py
@@ -1,10 +1,7 @@
 """Formats case citations for Label Studio."""
 
 import uuid
-from typing import cast
-from collections.abc import Callable
 
-from eyecite import get_citations, resolve_citations
 from eyecite.models import (
     CitationBase,
     FullCaseCitation,
@@ -39,9 +36,44 @@ class UnknownCitationError(Exception):
         """Initialize the exception class with message."""
         super().__init__(message)
 
+def _assign_id_to_citation(citations: list[CitationBase]) -> list[tuple[CitationBase, str]]:
+    """Assign unique IDs to citations.
 
-def create_bibliography(citations: list) -> dict:
-    """Format citations for Label Studio."""
+    Args:
+    ----
+        citations: a list of citations
+
+    Returns:
+    -------
+        a list of all citations with unique IDs
+    """
+    citations_with_id = []
+    # Check if citation is onIe of eyecite's
+    for cite in citations:
+        # Get the type
+        cls = type(cite)
+        if cls not in CITATION_TYPES:
+            raise UnknownCitationError("Ensure all citations types are")
+        citations_with_id.append((cite, str(uuid.uuid4())[:8]))
+    return citations_with_id
+
+def _get_antecedent_mapping(resolutions: dict[Resource, list[CitationBase]]):
+
+
+def create_bibliography(citations: list[CitationBase], resources: dict[Resource, list[CitationBase]]) -> dict:
+    """Format citations for Label Studio.
+
+    Args:
+    ----
+        citations: a list of citations
+        resources: a dict of resolved citations
+
+    Returns:
+    -------
+        A dict formatted for Label Studio
+    """
     result: list = []
+
+    antecedent_mapping = 
 
     return {"model_version": "TEMP:TODO", "score": 1.0, "result": result}
