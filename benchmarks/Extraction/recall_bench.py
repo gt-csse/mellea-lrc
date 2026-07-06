f5572086a311e9fe7aa629e25678bb4c9b214d14
ADRian123L <lozada22@icloud.com>
2026-07-05 23:22:05 -0500

    fix: metadata deserialization of data structures

--- benchmarks/Extraction/recall_bench.py
+++ benchmarks/Extraction/recall_bench.py
@@ -6,8 +6,11 @@ from pathlib import Path
 import sys
 from dataclasses import asdict
 import json
+from typing import cast
 
-from mellea_lrc.extraction import extract_document_file, extract_citations, DocumentExtraction
+from mellea_lrc.extraction import extract_document_file, extract_citations, DocumentExtraction, ExtractedCitation
+from mellea_lrc.core import CanonicalCitation, FullCaseCitation, FullLawCitation, FullJournalCitation, ShortCaseCitation, SupraCitation, IdCitation
+from mellea_lrc.preprocessing import PreprocessedDocument, PreprocessedDocumentMetadata
 
 # %%
 # Prints the sys.modules
@@ -48,9 +51,43 @@ def convert_to_text(file_path: Path) -> str:
 # Save the citations
 def save_citations(path: Path, document: DocumentExtraction) -> None:
     """Save the citions."""
+    document_dict = asdict(document)
+    # Add `kind` key to each Citation
+    for idx, citation in enumerate(document_dict.get("citations", [])):
+        citation["citation"]["kind"] = document.citations[idx].citation.kind
     with path.open("w", encoding="utf-8") as file:
-        json.dump(asdict(document), file, ident=4)
+        json.dump(document_dict, file, indent=4)
 
+# %%
+# Load the saved citations
+mapping = { citation_type.kind : citation_type for citation_type in (FullCaseCitation, FullJournalCitation, FullLawCitation, ShortCaseCitation, SupraCitation, IdCitation)}
+def load_citations(path: Path) -> DocumentExtraction:
+    """Load the saved citations."""
+    data = None
+    if path.exists() and path.is_file() and path.suffix == ".json":
+        with path.open("r", encoding="utf-8") as file:
+            data = json.load(file)
+    else:
+        msg = f"{path} doesn't exists, is not a file, or it isn't json"
+        raise Exception(msg)
+    citations = []
+    if data:
+        for citation in data.get("citations", []):
+            sub_citation = citation.pop("citation")
+            kind =  sub_citation.pop("kind") # raise error if key is missing (should be there)
+            citation_type = mapping[kind](**sub_citation) # Create a citation of 'kind' type
+            temp = ExtractedCitation(citation=citation_type, **citation)
+            citations.append(temp)
+        preprocessed = data.pop("preprocessed")
+        pre_pros_meta = PreprocessedDocumentMetadata(**preprocessed["metadata"])
+        pre_pros_doc = PreprocessedDocument(text=preprocessed["text"], metadata=pre_pros_meta)
+
+        citations = cast(tuple[ExtractedCitation, ...], citations)
+        document = DocumentExtraction(preprocessed=pre_pros_doc, citations=citations)
+        return document
+    else:
+        msg = f"Couldn't create DocumentExtraction for {path}"
+        raise Exception(msg)
 # %%
 def main() -> int:
     """Run the mellea benchmark."""
@@ -72,10 +109,32 @@ def main() -> int:
     header = "\n".join(text.split("\n")[:9])
     print(header)
 
-    # upload_schema.main()
-    # upload_tasks.main(paths=[str(text_path)]) Don't upload them to Label Studio
     citations: DocumentExtraction = extract_citations(text)
+    saved_document_path = dir_name / str(hash_string + ".json")
+    save_citations(saved_document_path, citations)
+    citation = load_citations(saved_document_path)
+    #assert(citation == citations)
+    separator = "-" * 80
+    if citations.text != citation.text:
+        print("The text are not equal")
+    if citations.preprocessed != citation.preprocessed:
+        print("the preprocessed are not equal!")
+        if citations.preprocessed.metadata != citation.preprocessed.metadata:
+            print(" - The metadata are not equal")
+            print()
+            x = json.dumps(asdict(citations.preprocessed.metadata), indent=4)
+            print(x)
+            print(separator)
+            print()
+            x = json.dumps(asdict(citation.preprocessed.metadata), indent=4)
+            print(x)
+            print(separator)
+            
 
+
+
+    if citations.citations != citation.citations:
+        print("the citations are not equal")
     return 0
 
 # %%
