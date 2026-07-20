6264f33ee4bc52d0e2dcbdbc36db2fa4e651a303
ADRian123L <lozada22@icloud.com>
2026-07-20 10:18:08 -0400

    chore(extraction): remove duplicate code.

--- src/mellea_lrc/helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Low-level helpers and utilities."""
-
-from .convert_to_label_studio_format import create_bibliography
-from .upload_label_studio import send_data
-
-__all__ = ["create_bibliography", "send_data"]
